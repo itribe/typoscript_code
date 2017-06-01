16adeae3db37ec4c586985341a67546993f41053
Anton Danilov <danilov.q@gmail.com>
2017-06-01 17:42:32 +0500

    [TASK] Fixing TYPO3 8 breaking changes and deprecated functions
    - Frontend requests no longer load ext_tables.php in requests
    - TCA changes migration

--- ext_tables.php
+++ ext_tables.php
@@ -1,14 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-/** @var string $_EXTKEY */
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
-    'Content',
-    'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang.xlf:plugins.title',
-    'EXT:' . $_EXTKEY . '/Resources/Public/Icons/tt_content_ts.png'
-);
-
 if (TYPO3_MODE === 'BE') {
 	/** @noinspection PhpUndefinedClassInspection */
 	/** @var \TYPO3\CMS\Core\Imaging\IconRegistry $prefaIconRegistry */
