315d02724fc151bf6fe195021d5a6891db69147e
Anton Danilov <danilov.q@gmail.com>
2022-11-23 16:20:19 +0500

    [BUG] Fixing "Undefined array key" PHP warnings
    Issue #19

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,8 @@
 <?php
 
 call_user_func(function () {
-    $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['showitem'] = '
+    $typesConfig = [
+        'showitem' => '
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.header;header,bodytext,
@@ -18,31 +19,23 @@ call_user_func(function () {
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
             rowDescription,
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-    ';
+    ',
+        'columnsOverrides' => []
+    ];
 
-    // Add type icon class
-    $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['typoscriptcode_content'] = 'extensions-typoscript_code-content';
     // Activate t3editor for tt_content type typoscriptcode_content if this type exists and t3editor is loaded
     if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('t3editor')
-        && isset($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content'])
-        && is_array($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content'])
     ) {
-        if (!isset($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides'])
-            || !is_array($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides'])) {
-            $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides'] = [];
-        }
-        if (!isset($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext'])
-            || !is_array($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext'])) {
-            $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext'] = [];
-        }
-        if (!isset($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext']['config'])
-            || !is_array($GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext']['config'])) {
-            $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext']['config'] = [];
-        }
-        $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext']['config']['renderType'] = 't3editor';
-        $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext']['config']['wrap'] = 'off';
-        $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext']['config']['format'] = 'typoscript';
-        $GLOBALS['TCA']['tt_content']['types']['typoscriptcode_content']['columnsOverrides']['bodytext']['label'] = 'LLL:EXT:typoscript_code/Resources/Private/Language/locallang.xlf:bodytext';
+        $typesConfig['columnsOverrides'] = [
+            'bodytext' => [
+                'label' => 'LLL:EXT:typoscript_code/Resources/Private/Language/locallang.xlf:bodytext',
+                'config' => [
+                    'renderType' => 't3editor',
+                    'wrap' => 'off',
+                    'format' => 'typoscript',
+                ],
+            ],
+        ];
     }
     // Register the plugin
     // @extensionScannerIgnoreLine
@@ -52,4 +45,15 @@ call_user_func(function () {
         'LLL:EXT:typoscript_code/Resources/Private/Language/locallang.xlf:plugins.title',
         'extensions-typoscript_code-content'
     );
+    \TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TCA']['tt_content'], [
+        'ctrl' => [
+            // Add type icon class
+            'typeicon_classes' => [
+                'typoscriptcode_content' => 'extensions-typoscript_code-content',
+            ],
+        ],
+        'types' => [
+            'typoscriptcode_content' => $typesConfig,
+        ],
+    ]);
 });
